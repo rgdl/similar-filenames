1fea6f9f06514701d04f05709740dbd4ad9bf665
rgdl <robert_delisle44@hotmail.com>
2022-06-19 21:40:00 +1000

    break into functions

--- similar_filenames.py
+++ similar_filenames.py
@@ -8,41 +8,46 @@ Defaults to current directory.
 import argparse
 from pathlib import Path
 from typing import Any
+from typing import Dict
 
 import numpy as np
 import simhash  # type: ignore
 from tqdm import tqdm  # type: ignore
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument(
-    "dir",
-    nargs="?",
-    default=Path(),
-    help="directory to begin search from",
-)
-parser.add_argument(
-    "--max-results",
-    "-n",
-    type=int,
-    default=1000,
-    help="maximum number of results to return",
-)
-parser.add_argument(
-    "--max-depth",
-    "-d",
-    type=int,
-    help="maximum number of subdirectories to traverse",
-    default=0,
-)
 
-args = parser.parse_args()
+def parse_arguments() -> Dict[str, Any]:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "dir",
+        nargs="?",
+        default=Path(),
+        help="directory to begin search from",
+    )
+    parser.add_argument(
+        "--max-results",
+        "-n",
+        type=int,
+        default=1000,
+        help="maximum number of results to return",
+    )
+    parser.add_argument(
+        "--max-depth",
+        "-d",
+        type=int,
+        help="maximum number of subdirectories to traverse",
+        default=0,
+    )
 
-dir_path = Path(args.dir)
-n_results = args.max_results
-max_depth = args.max_depth
+    args = parser.parse_args()
+
+    return {
+        "dir_path": Path(args.dir),
+        "n_results": args.max_results,
+        "max_depth": args.max_depth,
+    }
 
 
-def count_bits(n: np.ndarray) -> np.ndarray[Any, Any]:
+def count_bits(n: np.ndarray) -> np.ndarray:
     """
     No idea how this works. Copied from here:
     https://stackoverflow.com/questions/9829578/
@@ -56,50 +61,60 @@ def count_bits(n: np.ndarray) -> np.ndarray[Any, Any]:
     return n
 
 
-all_fn = [
-    fn
-    for fn in dir_path.glob("**/*")
-    if all(
-        [
-            not fn.is_dir(),
-            len(fn.parts) <= max_depth,
-            fn.name not in (".DS_Store",),
-        ]
+def main(dir_path, n_results, max_depth) -> None:
+    all_fn = [
+        fn
+        for fn in dir_path.glob("**/*")
+        if all(
+            [
+                not fn.is_dir(),
+                len(fn.parts) <= max_depth,
+                fn.name not in (".DS_Store",),
+            ]
+        )
+    ]
+    distinct_fn = set(fn.name for fn in all_fn)
+    hash_dict = {str(fn): simhash.Simhash(fn).value for fn in distinct_fn}
+
+    all_f1 = []
+    all_f2 = []
+    hash1 = []
+    hash2 = []
+
+    for i, f1 in tqdm(list(enumerate(all_fn)), desc="Hashing"):
+        for f2 in all_fn[i + 1 :]:
+            all_f1.append(f1)
+            all_f2.append(f2)
+            hash1.append(hash_dict[str(f1.name)])
+            hash2.append(hash_dict[str(f2.name)])
+
+    hash_diff = np.array(hash1, dtype=np.uint64) ^ np.array(
+        hash2, dtype=np.uint64
     )
-]
-distinct_fn = set(fn.name for fn in all_fn)
-hash_dict = {str(fn): simhash.Simhash(fn).value for fn in distinct_fn}
+    hash_diff_count = count_bits(hash_diff)
+    hash_diff_count_order = hash_diff_count.argsort()
 
-all_f1 = []
-all_f2 = []
-hash1 = []
-hash2 = []
+    n_results = min(n_results, len(all_f1))
 
-for i, f1 in tqdm(list(enumerate(all_fn)), desc="Hashing"):
-    for f2 in all_fn[i + 1 :]:
-        all_f1.append(f1)
-        all_f2.append(f2)
-        hash1.append(hash_dict[str(f1.name)])
-        hash2.append(hash_dict[str(f2.name)])
-
-hash_diff = np.array(hash1, dtype=np.uint64) ^ np.array(hash2, dtype=np.uint64)
-hash_diff_count = count_bits(hash_diff)
-hash_diff_count_order = hash_diff_count.argsort()
-
-n_results = min(n_results, len(all_f1))
-
-if n_results:
-    print()
-    print(n_results, "most similar pairs of filenames:")
-
-    for i in reversed(range(n_results)):
-        ind = hash_diff_count_order[i]
-        print("\n***\n")
-        print(all_f1[ind])
-        print(all_f2[ind])
+    if n_results:
         print()
-        print("# Similarity Hashes differ by", hash_diff_count[ind], "bits")
-    print()
-else:
-    print()
-    print("No files found")
+        print(n_results, "most similar pairs of filenames:")
+
+        for i in reversed(range(n_results)):
+            ind = hash_diff_count_order[i]
+            print("\n***\n")
+            print(all_f1[ind])
+            print(all_f2[ind])
+            print()
+            print(
+                "# Similarity Hashes differ by", hash_diff_count[ind], "bits"
+            )
+        print()
+    else:
+        print()
+        print("No files found")
+
+
+if __name__ == "__main__":
+    args_dict = parse_arguments()
+    main(**args_dict)
